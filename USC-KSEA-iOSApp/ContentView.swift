06d0d3b919f131d382f7f2c9aa0aac9bc61c50dc
hybin21 <yebin492@gmail.com>
2023-10-17 02:09:41 -0700

    used UIscreen and dunno if i have to use UIscreen for the icons too.

--- USC-KSEA-iOSApp/ContentView.swift
+++ USC-KSEA-iOSApp/ContentView.swift
@@ -7,14 +7,17 @@
 
 import SwiftUI
 
+let screenWidth = UIScreen.main.bounds.width
+let screenHeight = UIScreen.main.bounds.height
+
 struct ContentView: View {
     var body: some View {
-        GeometryReader{geometry in
-            ZStack{
-                VStack{ // background
+        GeometryReader { geometry in
+            ZStack {
+                VStack { // background
                     Rectangle()
                         .foregroundColor(.clear)
-                        .frame(width: 402, height: 352)
+                        .frame(width: screenWidth, height: screenHeight * 0.4)
                         .background(
                             Image("IMG_2038")
                                 .resizable()
@@ -22,49 +25,53 @@ struct ContentView: View {
                                 .edgesIgnoringSafeArea(.all)
                                 .overlay(
                                     Color(red: 0.62, green: 0.13, blue: 0.21).opacity(0.70)
-                                    // hello
                                 )
                         )
-                    Spacer().frame(height:503)
+                    Spacer().frame(height: screenHeight * 0.6)
                 }
-                Spacer()
                 
-                VStack{ // left menu icon
-                    Spacer().frame(height:28)
-                    HStack{
-                        Spacer().frame(width:24)
+                VStack {
+                    HStack {
                         Image("menu-01")
-                            .frame(width:24, height:24)
-                        Spacer().frame(width:128)
+                            .frame(width: 24, height: 24)
+                        
+                        Spacer()
+                        
                         Image("Sash Logo 1")
                             .resizable()
                             .aspectRatio(contentMode: .fill)
                             .frame(width: 42, height: 33)
                             .clipped()
-                        Spacer().frame(width:128)
-                        Image("bell-03")
-                            .frame(width:24,height:24)
+                        
                         Spacer()
+                        
+                        Image("bell-03")
+                            .frame(width: 24, height: 24)
                     }
+                    .padding(.horizontal, screenWidth * 0.05)
+                    
                     Spacer()
                 }
-                Text("KSEA USC") 
-                    .font(Font.custom("Mont", size: 30))
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(.white)
-                    .position(x: geometry.size.width / 2, y: 128)
-                    .bold()
                 
-                Text("Korean-American Scientists and Engineers Association at USC")
-                .font(Font.custom("Mont", size: 10))
-                .multilineTextAlignment(.center)
-                .position(x: geometry.size.width / 2, y: 160)
-                .foregroundColor(Color(red: 1, green: 0.78, blue: 0.17))
-                .bold()
-                
+                VStack {
+                    Spacer().frame(height: screenHeight * 0.2)
+                    
+                    Text("KSEA USC")
+                        .font(Font.custom("Mont", size: 30))
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(.white)
+                        .bold()
+                    
+                    Text("Korean-American Scientists and Engineers Association at USC")
+                        .font(Font.custom("Mont", size: 10))
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(Color(red: 1, green: 0.78, blue: 0.17))
+                        .bold()
+                    
+                    Spacer()
+                }
             }
         }
-        
     }
 }
 
