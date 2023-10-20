12e86ab2f7fd74451425cad16ff2568a1c03787e
Donggun Lee <donggunl@usc.edu>
2023-10-20 02:31:51 -0700

    Fixed footer. UIScreen removed b/c soon obsolete;

--- USC-KSEA-iOSApp/ContentView.swift
+++ USC-KSEA-iOSApp/ContentView.swift
@@ -9,89 +9,72 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        
-        VStack {
-            Spacer()
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            
-            Text("Hello World")
-                .bold().padding()
-                
-            Spacer()
-            
-            VStack(spacing: 0) {
-                // Yellow rectangle footer
-                Rectangle()
-                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-                    .foregroundColor(.clear)
-                    .frame(width: UIScreen.main.bounds.size.width, height: 16)
-                    .background(Color(red:1, green: 0.78, blue: 0.17))
 
-                // Red rectangle footer
-                ZStack {
-                    Rectangle()
-                        .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing:0))
-                        .foregroundColor(.clear)
-                        .frame(width: UIScreen.main.bounds.width, height: 74)
-                        .background(Color(red: 0.62, green: 0.13, blue: 0.21))
-                    HStack {
-                        HStack {
-                            Spacer()
-                            Image("home-01")
-                            //.frame(width: 35, height: 35)
-                            Spacer()
-                        }
-                        Rectangle()
-                            .foregroundColor(.clear)
-                            .frame(width: 1.5, height: 30)
-                            .background(.white)
-                        HStack {
-                            Spacer()
-                            Image("help-square-contained")
-                            //.frame(width: 35, height: 35)
-                            Spacer()
-                        }
-                        Rectangle()
-                            .foregroundColor(.clear)
-                            .frame(width: 1.5, height: 30)
-                            .background(.white)
-                        HStack {
-                            Spacer()
-                            Image("users-profiles-02")
-                            //.frame(width: 35, height: 35)
-                            Spacer()
-                        }
-                        Rectangle()
-                            .foregroundColor(.clear)
-                            .frame(width: 1.5, height: 30)
-                            .background(.white)
-                        HStack {
-                            Spacer()
-                            Image("image-02")
-                            //.frame(width: 35, height: 35)
-                            Spacer()
-                        }
-                        Rectangle()
-                            .foregroundColor(.clear)
-                            .frame(width: 1.5, height: 30)
-                            .background(.white)
-                        HStack {
-                            Spacer()
-                            Image("email")
-                            //.frame(width: 35, height: 35)
-                            Spacer()
-                        }
-                    }
-                }
-                
+        VStack(spacing: 0) {
+            ScrollView {
+                // contents
             }
+            Spacer()
+            // Yellow rectangle footer
+            Rectangle()
+                .foregroundColor(.clear)
+                .frame(height: 16)
+                .frame(maxWidth: .infinity)
+                .background(Color(red:1, green:0.78, blue: 0.17))
             
-                
-                
-        }
-        .padding()
+            ZStack {
+                Rectangle()
+                    .padding(EdgeInsets(top:0, leading:0, bottom:0, trailing:0))
+                    .foregroundColor(.clear)
+                    .frame(height: 74)
+                    .frame(width: .infinity)
+                    .background(Color(red:0.62, green: 0.13, blue: 0.21))
+                HStack {
+                    HStack{
+                        Spacer()
+                        Image("home-01")
+                        Spacer()
+                    }
+                    Rectangle()
+                        .foregroundColor(.clear)
+                        .frame(width: 1.5, height: 30)
+                        .background(.white)
+                    HStack {
+                        Spacer()
+                        Image("help-square-contained")
+                        Spacer()
+                    }
+                    Rectangle()
+                        .foregroundColor(.clear)
+                        .frame(width: 1.5, height: 30)
+                        .background(.white)
+                    HStack {
+                        Spacer()
+                        Image("users-profiles-02")
+                        Spacer()
+                    }
+                    Rectangle()
+                        .foregroundColor(.clear)
+                        .frame(width: 1.5, height: 30)
+                        .background(.white)
+                    HStack {
+                        Spacer()
+                        Image("image-02")
+                        Spacer()
+                    }
+                    Rectangle()
+                        .foregroundColor(.clear)
+                        .frame(width: 1.5, height: 30)
+                        .background(.white)
+                    HStack {
+                        Spacer()
+                        Image("email")
+                        Spacer()
+                    }
+                } // HStack
+            } // ZStack
+        } // VStack
+ 
     }
 }
 
