e8fa7bf6b16107f2ccc6fcfb9666ea793e53197d
hybin21 <yebin492@gmail.com>
2023-10-27 14:17:12 -0700

    plz work

--- USC-KSEA-iOSApp/ContentView.swift
+++ USC-KSEA-iOSApp/ContentView.swift
@@ -1,20 +1,13 @@
-//
-//  ContentView.swift
-//  USC-KSEA-iOSApp
-//
-//  Created by Danford Cho on 10/4/23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        GeometryReader{geometry in
-            ZStack{
-                VStack{ // background
+        GeometryReader { geometry in
+            ZStack {
+                VStack {
                     Rectangle()
                         .foregroundColor(.clear)
-                        .frame(width: 402, height: 352)
+                        .frame(width: geometry.size.width, height: geometry.size.height * 0.4)
                         .background(
                             Image("IMG_2038")
                                 .resizable()
@@ -24,49 +17,58 @@ struct ContentView: View {
                                     Color(red: 0.62, green: 0.13, blue: 0.21).opacity(0.70)
                                 )
                         )
-                    Spacer().frame(height:503)
+                    Spacer().frame(height: geometry.size.height * 0.6)
                 }
-                Spacer()
-                
-                VStack{ // left menu icon
-                    Spacer().frame(height:28)
-                    HStack{
-                        Spacer().frame(width:24)
+                // hello
+                VStack {
+                    HStack {
                         Image("menu-01")
-                            .frame(width:24, height:24)
-                        Spacer().frame(width:128)
+                            .resizable()
+                            .frame(width: geometry.size.width * 0.06, height: geometry.size.width * 0.06)
+                        
+                        Spacer()
+                        
                         Image("Sash Logo 1")
                             .resizable()
                             .aspectRatio(contentMode: .fill)
-                            .frame(width: 42, height: 33)
+                            .frame(width: geometry.size.width * 0.10, height: geometry.size.width * 0.08)
                             .clipped()
-                        Spacer().frame(width:128)
-                        Image("bell-03")
-                            .frame(width:24,height:24)
+                        
                         Spacer()
+                        
+                        Image("bell-03")
+                            .resizable()
+                            .frame(width: geometry.size.width * 0.06, height: geometry.size.width * 0.06)
                     }
+                    .padding(.horizontal, geometry.size.width * 0.05)
+                    
                     Spacer()
                 }
-                Text("KSEA USC") 
-                    .font(Font.custom("Mont", size: 30))
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(.white)
-                    .position(x: geometry.size.width / 2, y: 128)
-                    .bold()
                 
-                Text("Korean-American Scientists and Engineers Association at USC")
-                .font(Font.custom("Mont", size: 10))
-                .multilineTextAlignment(.center)
-                .position(x: geometry.size.width / 2, y: 160)
-                .foregroundColor(Color(red: 1, green: 0.78, blue: 0.17))
-                .bold()
-                
+                VStack {
+                    Spacer().frame(height: geometry.size.height * 0.2)
+                    
+                    Text("KSEA USC")
+                        .font(Font.custom("Mont", size: 30))
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(.white)
+                        .bold()
+                    
+                    Text("Korean-American Scientists and Engineers Association at USC")
+                        .font(Font.custom("Mont", size: 10))
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(Color(red: 1, green: 0.78, blue: 0.17))
+                        .bold()
+                    
+                    Spacer()
+                }
             }
         }
-        
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
