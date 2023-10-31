f36d7bbb3aae7d53b590a606cf81930cf54224f7
Jae Cho <jaehcho@usc.edu>
2023-10-31 15:26:42 -0700

    added header file

---
+++ USC-KSEA-iOSApp/Header.swift
@@ -0,0 +1,18 @@
+//
+//  Header.swift
+//  USC-KSEA-iOSApp
+//
+//  Created by Danford Cho on 10/31/23.
+//
+
+import SwiftUI
+
+struct Header: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Header()
+}
